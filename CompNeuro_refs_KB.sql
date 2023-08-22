15a7a64bbb915f6096e95f58491a2ed6a715d8ec
vivkow <viiwiiviwi@gmail.com>
2023-08-22 19:59:35 +0200

    Minor changes

--- CompNeuro_refs_KB.sql
+++ CompNeuro_refs_KB.sql
@@ -3,3 +3,8 @@ CREATE TABLE cn_refs AS
 SELECT *
 FROM wos_b_202304.refs
 WHERE item_id_citing IN ('WOS:XXXXX');
+
+-- OR, IF IMPORT OF CSV IS POSSIBLE,
+SELECT *
+FROM wos_b_202304.refs
+WHERE item_id_citing IN (SELECT item_id FROM CSVREAD('item_ids.csv')) 
